8bd640d8920843edf573fbd79ae38bd1e1c8b9e9
wahsandaruwan <wahsandaruwan6@gmail.com>
2020-09-08 00:40:53 +0530

    Get basic info of github users

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -11,7 +11,28 @@ window.onload = function(){
         xhr.open("GET","https://api.github.com/users/"+userName+"?client_id="+cl_id+"&client_secret="+cl_sec, true);
         xhr.onload = function(){
             if(this.status == 200){
-                console.log(this.responseText);
+                let user = JSON.parse(this.responseText);
+                // Set Name
+                document.getElementById("name").innerHTML = user.name;
+                // Set Image
+                document.getElementById("primg").src = user.avatar_url;
+                // Set Company
+                document.getElementById("company").innerHTML = user.company;
+                // Set Blog
+                document.getElementById("blog").innerHTML = user.blog;
+                // Set Location
+                document.getElementById("location").innerHTML = user.location;
+                // Set Email
+                document.getElementById("email").innerHTML = user.email;
+                // Set Hireable
+                if(user.hireable == true){
+                    document.getElementById("hire").style.backgroundColor = "#27ae60"
+                    document.getElementById("hire").innerHTML = "Hireable";
+                }
+                else{
+                    document.getElementById("hire").style.backgroundColor = "#e74c3c"
+                    document.getElementById("hire").innerHTML = "Not Hireable";
+                }
             }
         };
         xhr.send();
