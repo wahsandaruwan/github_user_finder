b7c63fc3f89e8e1a7270a1630d761883cbaafe6c
wahsandaruwan <wahsandaruwan6@gmail.com>
2020-09-11 00:14:28 +0530

    Add a modal for contact me

--- resources/js/modals.js
+++ resources/js/modals.js
@@ -1,11 +1,25 @@
+// For About Me
 // Open About Me modal when clicks about me link
 document.getElementById("ab").addEventListener("click", function(event){
     event.preventDefault();
-    document.getElementById("mc").classList.add("show-modal");
+    document.getElementById("ma").classList.add("show-modal");
 });
 
 // Close Modal when click close button
 document.getElementById("closea").addEventListener("click", function(event){
     event.preventDefault();
+    document.getElementById("ma").classList.remove("show-modal");
+});
+
+// For Contact Me
+// Open Contact Me modal when clicks contact me link
+document.getElementById("con").addEventListener("click", function(event){
+    event.preventDefault();
+    document.getElementById("mc").classList.add("show-modal");
+});
+
+// Close Modal when click close button
+document.getElementById("closec").addEventListener("click", function(event){
+    event.preventDefault();
     document.getElementById("mc").classList.remove("show-modal");
-})
+});
