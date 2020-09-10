269a0b729fa0c24e279c77cb44f2d31258eec80c
wahsandaruwan <wahsandaruwan6@gmail.com>
2020-09-11 01:23:40 +0530

    Update about and contact modals

--- resources/js/modals.js
+++ resources/js/modals.js
@@ -1,5 +1,5 @@
-// For About Me
-// Open About Me modal when clicks about me link
+// For About
+// Open About modal when clicks about link
 document.getElementById("ab").addEventListener("click", function(event){
     event.preventDefault();
     document.getElementById("ma").classList.add("show-modal");
@@ -11,8 +11,8 @@ document.getElementById("closea").addEventListener("click", function(event){
     document.getElementById("ma").classList.remove("show-modal");
 });
 
-// For Contact Me
-// Open Contact Me modal when clicks contact me link
+// For Contact
+// Open Contact modal when clicks contact link
 document.getElementById("con").addEventListener("click", function(event){
     event.preventDefault();
     document.getElementById("mc").classList.add("show-modal");
