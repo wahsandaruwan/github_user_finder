066a098c3df6d9449ea2f4f14ebb7d8ea5400bb7
wahsandaruwan <wahsandaruwan6@gmail.com>
2020-09-15 20:29:14 +0530

    Added SVG logo with animations

--- resources/js/darkmode.js
+++ resources/js/darkmode.js
@@ -1,13 +1,35 @@
 // For Dark Mode
+var i = 0;
 document.getElementById("dark").addEventListener("click", function(event){
     event.preventDefault();
+    // Activate and Deactivate Dark Mode Class
     document.querySelectorAll(".darkm").forEach((result) =>{
         result.classList.toggle("dark-mode");
     });
-
-    // Fix positioning issue in footer
+    
+    // Fix positioning issue in footer by activating and deactivating invert class
     document.querySelectorAll(".foo").forEach((result) =>{
         result.classList.toggle("invert");
     });
+
+    // For identify if dark button is pressed or not
+    // First set i  to 1 when setting dark theme
+    if(i == 0){
+        i = 1;
+        console.log(i);
+    }
+    // Set i to 0 when unsetting dark theme
+    else{
+        i = 0;
+    }
 });
+
+// Function to set Dark Mode to SVG Logo
+function svg(){
+    if(i == 1){
+        document.querySelectorAll(".ro").forEach((result) =>{
+            result.classList.toggle("dark-mode");
+        });
+    }
+}
 
